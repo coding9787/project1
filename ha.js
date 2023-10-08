1ff34e1084779216030af4e3726a0686f2ffd909
coding9787 <coding9787@gmail.com>
2023-10-08 12:16:46 +0530

    user_info.html file update

--- ha.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBpoL5xVUayIeAGtmbbNVgpjMmfO3V4tjY",
-    authDomain: "webaut-9abc2.firebaseapp.com",
-    databaseURL: "https://webaut-9abc2-default-rtdb.firebaseio.com",
-    projectId: "webaut-9abc2",
-    storageBucket: "webaut-9abc2.appspot.com",
-    messagingSenderId: "652373857140",
-    appId: "1:652373857140:web:564b3b7122cee56f571221"
-  };
