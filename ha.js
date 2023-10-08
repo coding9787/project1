cc3de07a2201170081413dbd11a5d4f613c60bd4
coding9787 <coding9787@gmail.com>
2023-10-08 12:15:43 +0530

    user_info.html file updated

--- ha.js
+++ ha.js
@@ -1,22 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpoL5xVUayIeAGtmbbNVgpjMmfO3V4tjY",
-  authDomain: "webaut-9abc2.firebaseapp.com",
-  databaseURL: "https://webaut-9abc2-default-rtdb.firebaseio.com",
-  projectId: "webaut-9abc2",
-  storageBucket: "webaut-9abc2.appspot.com",
-  messagingSenderId: "652373857140",
-  appId: "1:652373857140:web:564b3b7122cee56f571221"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-
-
+    apiKey: "AIzaSyBpoL5xVUayIeAGtmbbNVgpjMmfO3V4tjY",
+    authDomain: "webaut-9abc2.firebaseapp.com",
+    databaseURL: "https://webaut-9abc2-default-rtdb.firebaseio.com",
+    projectId: "webaut-9abc2",
+    storageBucket: "webaut-9abc2.appspot.com",
+    messagingSenderId: "652373857140",
+    appId: "1:652373857140:web:564b3b7122cee56f571221"
+  };
